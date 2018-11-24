1edcece0103b374263f8d985ca9b63623531ab80
blackharts <35812381+blackharts@users.noreply.github.com>
2018-11-24 15:18:33 -0300

    actualizacion

--- PizarraDeVuelo/IngresarPiloto.Designer.cs
+++ PizarraDeVuelo/IngresarPiloto.Designer.cs
@@ -28,12 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_nombres = new System.Windows.Forms.TextBox();
+            this.txt_apellidos = new System.Windows.Forms.TextBox();
+            this.bt_guardar = new System.Windows.Forms.Button();
+            this.bt_cancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombres";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(41, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellidos";
+            // 
+            // txt_nombres
+            // 
+            this.txt_nombres.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_nombres.Location = new System.Drawing.Point(44, 84);
+            this.txt_nombres.Name = "txt_nombres";
+            this.txt_nombres.Size = new System.Drawing.Size(240, 29);
+            this.txt_nombres.TabIndex = 2;
+            // 
+            // txt_apellidos
+            // 
+            this.txt_apellidos.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_apellidos.Location = new System.Drawing.Point(44, 185);
+            this.txt_apellidos.Name = "txt_apellidos";
+            this.txt_apellidos.Size = new System.Drawing.Size(240, 29);
+            this.txt_apellidos.TabIndex = 3;
+            // 
+            // bt_guardar
+            // 
+            this.bt_guardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_guardar.Location = new System.Drawing.Point(44, 308);
+            this.bt_guardar.Name = "bt_guardar";
+            this.bt_guardar.Size = new System.Drawing.Size(112, 31);
+            this.bt_guardar.TabIndex = 4;
+            this.bt_guardar.Text = "Guardar";
+            this.bt_guardar.UseVisualStyleBackColor = true;
+            // 
+            // bt_cancel
+            // 
+            this.bt_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_cancel.Location = new System.Drawing.Point(162, 308);
+            this.bt_cancel.Name = "bt_cancel";
+            this.bt_cancel.Size = new System.Drawing.Size(112, 31);
+            this.bt_cancel.TabIndex = 5;
+            this.bt_cancel.Text = "Cancelar";
+            this.bt_cancel.UseVisualStyleBackColor = true;
+            // 
+            // IngresarPiloto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(332, 380);
+            this.Controls.Add(this.bt_cancel);
+            this.Controls.Add(this.bt_guardar);
+            this.Controls.Add(this.txt_apellidos);
+            this.Controls.Add(this.txt_nombres);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "IngresarPiloto";
             this.Text = "IngresarPiloto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_nombres;
+        private System.Windows.Forms.TextBox txt_apellidos;
+        private System.Windows.Forms.Button bt_guardar;
+        private System.Windows.Forms.Button bt_cancel;
     }
 }
